aeb796764ef420b682441566ecf3efe1cc2738d9
Superpiefac3 <40869436+Superpiefac3@users.noreply.github.com>
2023-09-26 11:13:41 -0400

    added pressActionKey function

--- src/models/calculator.model.ts
+++ src/models/calculator.model.ts
@@ -18,7 +18,28 @@ export class CalculatorModel implements ICalculatorModel {
   }
 
   public pressActionKey(key: ActionKeys): void {
-    throw new Error('Method not implemented.');
+    if(key == ActionKeys.CLEAR) {
+      this._buffer = ""
+    } else if (key == ActionKeys.EQUALS) {
+      this._buffer += '='
+      var eq = this._buffer.split("+").join(" ").split("*").join(" ").split("-").join(" ").split("/").join(" ").split(" ") 
+      var num1 = +eq[0]
+      var num2 = +eq[2]
+      switch(this._buffer.charAt(eq[0].length + 1)) {
+        case('+'):
+          this._buffer += num1 + num2
+          break;
+        case('-'):
+          this._buffer += num1 - num2
+          break;
+        case('*'):
+          this._buffer += num1 * num2
+          break;
+        case('/'):
+          this._buffer += num1 / num2
+          break;
+      }
+    }
   }
 
   public display(): string {
